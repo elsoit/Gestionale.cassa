fdcd0fc3e3bbb804fdb9b0182b0e3ba96898f908
elsayed alorabi <elsayedalorabi@MacBook-Air-di-elsayed-2.local>
2025-01-27 15:46:46 +0100

    cors

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@ const nextConfig = {
       ? 'https://api2.artexmoda.com'  
       : 'http://localhost:3003'
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
+          { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+        ],
+      },
+    ]
+  },
   images: {
     remotePatterns: [
       {
